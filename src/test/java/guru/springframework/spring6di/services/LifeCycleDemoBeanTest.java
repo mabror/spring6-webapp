87a2be9d7a68b4a69c95829bff2ed6f0b859b88e
Abror Mamadaliev <mabror2001@gmail.com>
2024-02-29 12:07:05 +0500

    Dependency Injection

---
+++ src/test/java/guru/springframework/spring6di/services/LifeCycleDemoBeanTest.java
@@ -0,0 +1,48 @@
+package guru.springframework.spring6di.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LifeCycleDemoBeanTest {
+
+    @Test
+    void setJavaVer() {
+    }
+
+    @Test
+    void setBeanName() {
+    }
+
+    @Test
+    void setBeanFactory() {
+    }
+
+    @Test
+    void setApplicationContext() {
+    }
+
+    @Test
+    void postConstruct() {
+    }
+
+    @Test
+    void afterPropertiesSet() {
+    }
+
+    @Test
+    void preDestroy() {
+    }
+
+    @Test
+    void destroy() {
+    }
+
+    @Test
+    void postProcessBeforeInitialization() {
+    }
+
+    @Test
+    void postProcessAfterInitialization() {
+    }
+}
